3bb22f2fdef46cd0f42d63ad12abf430a56eb7b9
Norwedish <remco-bruijn@hotmail.com>
2023-07-24 17:16:50 +0200

    added an average of 3 with state and last count

--- gethenkies.py
+++ gethenkies.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import math
 from datetime import datetime
 
 def get_total_headless_henk_versions(data):
@@ -21,49 +22,57 @@ def get_total_userscripts(data):
     return total_userscripts
 
 def main():
-    previous_henkies_count = 0
-    previous_userscript_count = 0
-    previous_difference_count = 0
+    previous_henkies_count = [0, 0, 0]
+    previous_userscript_count = [0, 0, 0]
+    previous_difference_count = [0, 0, 0]
 
     while True:
         try:
             response = requests.get('https://chief.placenl.nl/api/stats')
             data = response.json()
 
+            prev_henkies_calc = (math.floor((previous_henkies_count[0]+previous_henkies_count[1]+previous_henkies_count[2]) / 3))
+            prev_userscript_calc = (math.floor((previous_userscript_count[0]+previous_userscript_count[1]+previous_userscript_count[2]) / 3))
+            prev_diff_calc = (math.floor((previous_difference_count[0]+previous_difference_count[1]+previous_difference_count[2]) / 3))
+
             total_headless_henk = get_total_headless_henk_versions(data)
-            henk_change = "up" if total_headless_henk > previous_henkies_count else "down"
-            if total_headless_henk == previous_henkies_count:
+            henk_change = "up" if total_headless_henk > prev_henkies_calc else "down"
+            if total_headless_henk == previous_henkies_count[0]:
                 henk_change = "same"
 
             total_userscripts = get_total_userscripts(data)
-            userscript_change = "up" if total_userscripts > previous_userscript_count else "down"
-            if total_userscripts == previous_userscript_count:
+            userscript_change = "up" if total_userscripts > prev_userscript_calc else "down"
+            if total_userscripts == previous_userscript_count[0]:
                 userscript_change = "same"
 
             difference = (abs(total_headless_henk - total_userscripts))
-            difference_change = "up" if difference > previous_difference_count else "down"
-            if difference == previous_difference_count:
+            difference_change = "up" if difference > prev_diff_calc else "down"
+            if difference == previous_difference_count[0]:
                 difference_change = "same"
 
-            previous_henkies_count = total_headless_henk
-            previous_userscript_count = total_userscripts
-            previous_difference_count = difference
 
             # Clear previous output
             print("\033[H\033[J", end='')
 
             current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             print(f'Current Date and Time: {current_datetime}')
-            print(f'Total Headless-Henk versions: {total_headless_henk} {henk_change}')
-            print(f'Total Userscripts: {total_userscripts} {userscript_change}')
-            print(f'Difference: {difference} {difference_change}')
 
-            old_headless_henk = total_headless_henk
-            old_userscripts = total_userscripts
+            print(f'Total Headless-Henk versions:	{total_headless_henk},	last count:	{previous_henkies_count[0]},	average of 3:	{prev_henkies_calc},	state:	{henk_change}')
+            print(f'Total Userscripts:		{total_userscripts},	last count:	{previous_userscript_count[0]},	average of 3:	{prev_userscript_calc},	state:	{userscript_change}')
+            print(f'Difference:			{difference},	last count:	{previous_difference_count[0]},	average of 3:	{prev_diff_calc},	state:	{difference_change}')
+
+            previous_henkies_count.pop(2)
+            previous_henkies_count.insert(0, total_headless_henk)
+
+            previous_userscript_count.pop(2)
+            previous_userscript_count.insert(0, total_userscripts)
+
+            previous_difference_count.pop(2)
+            previous_difference_count.insert(0, difference)
         except Exception as e:
             print(f'Error occurred: {e}')
 
-        time.sleep(5)
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
